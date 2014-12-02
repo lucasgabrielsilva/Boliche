72152d4b2a37599c95207d9a3822cff577e93201
LUCAS GABRIEL DA SILVA <a1243640@06e104-507903.(none)>
2014-12-02 21:22:17 -0200

    boliche3

--- src/jogodeboliche/Boliche.java
+++ src/jogodeboliche/Boliche.java
@@ -4,23 +4,58 @@ package jogodeboliche;
 
 
 public class Boliche {
+
      int score = 0;
-     int pinos;
 
+     int jogadas[] = new int[21];
+     
+     int p = 0;
+     
+     int ultimoSpare = 0;
+     
+     int STRIKE = 0;
 
+     public Boliche(){
+         
+     }
     public int score() {
         return score;
     }
 
     public void roll(int pinos) {
         if(pinos >0 && pinos <11){
+            if(pinos==10){
+                STRIKE = 2;
+                jogadas[p] = 10;
+                jogadas[p+1] = 0;
+                p+=2;
+                
+            }
+            else{
+                if(STRIKE !=0){
+                          STRIKE --;
+                          this.score += pinos;
+                }
+                if(p%2==0){
+                    int soma =0;
+                    for (int i = ultimoSpare;i < p; i++) {
+                        soma += jogadas[i];
+                    }
+                    if(soma == 10){
+                        this.ultimoSpare = p;
+                        this.score += pinos;
+                    }
+                }
+                jogadas[p] = pinos;
+            
+            }
             this.score += pinos;
+            p++;
         }
         else{
            throw new IllegalArgumentException();
         }
       
     }
-    
     
 }
